adc63f86d92d5631c2950d533f5e0383118166cc
crystw <crystal.angi.wang@gmail.com>
2025-05-11 19:36:03 -0500

    Update LogIn.js

--- pages/LogIn.js
+++ pages/LogIn.js
@@ -10,7 +10,9 @@ export function LogIn({ navigation }) {
         accounts: [
             {
                 name: 'Username1',
-                pfp: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/480px-Default_pfp.svg.png'
+                pfp: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/480px-Default_pfp.svg.png',
+                email: 'email@gmail.com',
+                numProjects: 2
             }
         ],
     
@@ -26,6 +28,18 @@ export function LogIn({ navigation }) {
   return (
     <View style={styles.container}>
         <View style={{display: this.state.loginDisplay}}>
+            <View style={styles.backButtonContainer}>
+                    <TouchableHighlight
+                        onPress={() => navigation.replace('Accounts')}
+                    >
+                                
+                        <View style={styles.backButton}>
+                            <Text style={styles.buttonText}>
+                                Back
+                            </Text>
+                        </View>
+                    </TouchableHighlight>
+            </View>
             <View style={styles.contentContainer}>
                 <Text style={styles.titleText}>
                     Login
@@ -72,10 +86,21 @@ const styles = StyleSheet.create({
         backgroundColor: '#1d3557',
     },
     contentContainer: {
-        height: 14 * deviceHeight / 15,
+        height: 13 * deviceHeight / 15,
         width: 16 * deviceWidth / 18,
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    backButtonContainer: {
+        height: 1 * deviceHeight / 15,
+    },
+    backButton: {
+        backgroundColor: '#a8dadc',
+        height: 1 * deviceHeight / 15,
+        width: 2 * deviceWidth / 7,
+        borderRadius: 20,
+        alignItems: 'center',
+        justifyContent: 'center'
     },
     button1: {
         backgroundColor: '#a8dadc',
@@ -118,9 +143,77 @@ const styles = StyleSheet.create({
     },
     headerContainer: {
         height: deviceHeight / 10,
+        width: 16 * deviceWidth / 18,
         flexDirection: 'row',
         alignItems: 'center',
-        justifyContent: 'space-around'
+        justifyContent: 'space-between',
+    },
+    userProfileContainer: {
+        height: deviceHeight / 20,
+        width: 2 * deviceWidth / 7,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    projectsButton: {
+        height: deviceHeight / 20,
+        width: deviceWidth / 4,
+        backgroundColor: '#415a77',
+        borderRadius: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    codeButtonsContainer: {
+        height: deviceHeight / 9,
+        width: 16 * deviceWidth / 18,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    saveButton: {
+        height: deviceHeight / 20,
+        width: deviceWidth / 3,
+        backgroundColor: '#a8dadc',
+        borderRadius: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    runButton: {
+        height: deviceHeight / 20,
+        width: deviceWidth / 3,
+        backgroundColor: '#e63946',
+        borderRadius: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    homeContainer: {
+        height: 6 * deviceHeight / 20,
+        width: 16 * deviceWidth / 18,
+        marginBottom: 10
+    },
+    titleBarContainer: {
+        height: deviceHeight / 20,
+        width: 16 * deviceWidth / 18,
+        backgroundColor: 'white',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+    },
+    codeContainer: {
+        height: 5 * deviceHeight / 20,
+        width: 16 * deviceWidth / 18,
+        backgroundColor: '#778da9',
+        borderBottomLeftRadius: 10,
+        borderBottomRightRadius: 10,
+    },
+    profileContainer: {
+        height: 6 * deviceHeight / 10,
+        width: 3 * deviceWidth / 4,
+        backgroundColor: '#034774',
+        justifyContent: 'space-around',
+        marginTop: 25,
     },
     titleText: {
         color: 'white',
@@ -128,14 +221,6 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         fontSize: deviceHeight / 13,
         marginBottom: 10,
-    },
-    titleText2: {
-        color: 'white',
-        fontFamily: 'fira code',
-        fontWeight: 'bold',
-        fontSize: deviceHeight / 20,
-        textAlign: 'center',
-        marginBottom: 10,   
     },
     buttonText: {
         color: '#457b9d',
@@ -149,13 +234,32 @@ const styles = StyleSheet.create({
         fontSize: deviceHeight / 28,
         marginLeft: 10,
     },
-    authText: {
+    usernameText: {
+        color: '#457b9d',
+        fontFamily: 'fira code',
+        fontWeight: 'bold',
+        fontSize: deviceHeight / 35
+    },
+    projectsButtonText: {
         color: 'white',
         fontFamily: 'fira code',
-        fontSize: deviceHeight / 30,
-        textAlign: 'center',
-        marginBottom: 10, 
+        fontSize: deviceHeight / 37
+    },
+    buttonText2: {
+        color: 'white',
+        fontFamily: 'fira code',
+        fontSize: deviceHeight / 35
+    },
+    titleBarText: {
+        color: '#457b9d',
+        fontFamily: 'fira code',
+        fontSize: deviceHeight / 35,
+        marginLeft: 10
+    },
+    profileText: {
+        color: '#778da9',
+        fontFamily: 'fira code',
+        fontSize: deviceHeight / 20,
+        marginLeft: 10,
     }
-    
-    
 });
