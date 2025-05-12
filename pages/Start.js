f5677bd32968b0d4ef04e790176e9347fd8ed8c8
Adya Gopi Bharadwaj <adyagopibharadwaj@gmail.com>
2025-05-11 22:23:24 -0500

    Add files via upload
    1. Removed hardcoded user name and other user attributes in Start.js. Now it lists Login and Sign up buttons.
    2. Signup takes in the user attributes and saves them to Back4App accounts table using App ID and Javascript key
    
    3. Login page retrieves the verifies the user credentials with Back4App user records

--- pages/Start.js
+++ pages/Start.js
@@ -1,68 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Dimensions, StyleSheet, Text, TouchableHighlight, View } from 'react-native';
 
 let deviceHeight = Dimensions.get('window').height;
 let deviceWidth = Dimensions.get('window').width;
 
 export function Start({ navigation }) {
-
-  state= {
-    accounts: [
-        {
-            name: 'Username1',
-            pfp: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/480px-Default_pfp.svg.png',
-            email: 'email@gmail.com',
-            numProjects: 2
-        }
-    ],
-
-    loginUsername: 'Username',
-    loginPassword: 'Password',
-    signUpUsername: 'Username',
-    signUpEmail: 'Email',
-    signUpPassword: 'Password',
-    signUpConfirmPassword: 'Confirm Password',
-    authCode: 'Code'
-  }
-
-  handleLoginUsernameInput = loginUsername => {
-    this.setState({ loginUsername });
-  };
-
-  handleLoginPasswordInput = loginPassword => {
-      this.setState({ loginPassword });
-  };
-
-  handleSignUpUsernameInput = signUpUsername => {
-      this.setState({ signUpUsername });
-  };
-
-  handleSignUpEmailInput = signUpEmail => {
-      this.setState({ signUpEmail });
-  };
-
-  handleSignUpPasswordInput = signUpPassword => {
-      this.setState({ signUpPassword });
-  };
-
-  handleSignUpConfirmPasswordInput = signUpConfirmPassword => {
-      this.setState({ signUpConfirmPassword });
-  };
-
-  handleAuthCodePasswordInput = authCode => {
-      this.setState({ authCode });
-  };
-
+    const [startDisplay, setStartDisplay] = useState('flex');
+  
   return (
     <View style={styles.container}>
-        <View style={{display: this.state.startDisplay}}>
+        <View style={{display: startDisplay}}>
             <View style={styles.contentContainer}>
                 <Text style={styles.titleText}>
                     pseudo-lang
                 </Text>
                             
                 <TouchableHighlight
-                    onPress={() => navigation.replace('Accounts')}
+                    onPress={() => navigation.replace('Log In')}
                 >
                     <View style={styles.button1}>
                         <Text style={styles.buttonText}>
@@ -95,21 +49,10 @@ const styles = StyleSheet.create({
         backgroundColor: '#1d3557',
     },
     contentContainer: {
-        height: 13 * deviceHeight / 15,
+        height: 14 * deviceHeight / 15,
         width: 16 * deviceWidth / 18,
         alignItems: 'center',
         justifyContent: 'center',
-    },
-    backButtonContainer: {
-        height: 1 * deviceHeight / 15,
-    },
-    backButton: {
-        backgroundColor: '#a8dadc',
-        height: 1 * deviceHeight / 15,
-        width: 2 * deviceWidth / 7,
-        borderRadius: 20,
-        alignItems: 'center',
-        justifyContent: 'center'
     },
     button1: {
         backgroundColor: '#a8dadc',
@@ -152,77 +95,9 @@ const styles = StyleSheet.create({
     },
     headerContainer: {
         height: deviceHeight / 10,
-        width: 16 * deviceWidth / 18,
         flexDirection: 'row',
         alignItems: 'center',
-        justifyContent: 'space-between',
-    },
-    userProfileContainer: {
-        height: deviceHeight / 20,
-        width: 2 * deviceWidth / 7,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-    },
-    projectsButton: {
-        height: deviceHeight / 20,
-        width: deviceWidth / 4,
-        backgroundColor: '#415a77',
-        borderRadius: 20,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    codeButtonsContainer: {
-        height: deviceHeight / 9,
-        width: 16 * deviceWidth / 18,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-    },
-    saveButton: {
-        height: deviceHeight / 20,
-        width: deviceWidth / 3,
-        backgroundColor: '#a8dadc',
-        borderRadius: 20,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    runButton: {
-        height: deviceHeight / 20,
-        width: deviceWidth / 3,
-        backgroundColor: '#e63946',
-        borderRadius: 20,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    homeContainer: {
-        height: 6 * deviceHeight / 20,
-        width: 16 * deviceWidth / 18,
-        marginBottom: 10
-    },
-    titleBarContainer: {
-        height: deviceHeight / 20,
-        width: 16 * deviceWidth / 18,
-        backgroundColor: 'white',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        borderTopLeftRadius: 10,
-        borderTopRightRadius: 10,
-    },
-    codeContainer: {
-        height: 5 * deviceHeight / 20,
-        width: 16 * deviceWidth / 18,
-        backgroundColor: '#778da9',
-        borderBottomLeftRadius: 10,
-        borderBottomRightRadius: 10,
-    },
-    profileContainer: {
-        height: 6 * deviceHeight / 10,
-        width: 3 * deviceWidth / 4,
-        backgroundColor: '#034774',
-        justifyContent: 'space-around',
-        marginTop: 25,
+        justifyContent: 'space-around'
     },
     titleText: {
         color: 'white',
@@ -230,6 +105,14 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         fontSize: deviceHeight / 13,
         marginBottom: 10,
+    },
+    titleText2: {
+        color: 'white',
+        fontFamily: 'fira code',
+        fontWeight: 'bold',
+        fontSize: deviceHeight / 20,
+        textAlign: 'center',
+        marginBottom: 10,   
     },
     buttonText: {
         color: '#457b9d',
@@ -243,32 +126,13 @@ const styles = StyleSheet.create({
         fontSize: deviceHeight / 28,
         marginLeft: 10,
     },
-    usernameText: {
-        color: '#457b9d',
-        fontFamily: 'fira code',
-        fontWeight: 'bold',
-        fontSize: deviceHeight / 35
-    },
-    projectsButtonText: {
+    authText: {
         color: 'white',
         fontFamily: 'fira code',
-        fontSize: deviceHeight / 37
-    },
-    buttonText2: {
-        color: 'white',
-        fontFamily: 'fira code',
-        fontSize: deviceHeight / 35
-    },
-    titleBarText: {
-        color: '#457b9d',
-        fontFamily: 'fira code',
-        fontSize: deviceHeight / 35,
-        marginLeft: 10
-    },
-    profileText: {
-        color: '#778da9',
-        fontFamily: 'fira code',
-        fontSize: deviceHeight / 20,
-        marginLeft: 10,
+        fontSize: deviceHeight / 30,
+        textAlign: 'center',
+        marginBottom: 10, 
     }
+    
+    
 });
