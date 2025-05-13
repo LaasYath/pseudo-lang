51aaeb1196775aeae451d8543aa20a5cf67e9945
crystw <crystal.angi.wang@gmail.com>
2025-05-13 01:19:17 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
 
 const Stack = createNativeStackNavigator();
 
